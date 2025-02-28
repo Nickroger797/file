d7ef9f2b98c87a5835d6ffab4f4ca013059ec363
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-02-28 21:16:35 +0530

    Update server.py

--- server.py
+++ server.py
@@ -7,4 +7,4 @@ def home():
     return "Bot is running!", 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, threaded=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
