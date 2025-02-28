a3a34ffa18deacce5aff12d27016bf373590a48d
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-02-28 11:15:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
+from database.database import db  # Import MongoDB
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Nickroger797'
 
+# Route to fetch all users from MongoDB
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = list(db.users.find({}, {"_id": 0, "username": 1}))
+    return jsonify(users)
 
 if __name__ == "__main__":
     app.run()
