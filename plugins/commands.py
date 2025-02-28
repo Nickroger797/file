46bf3e017c9f49db94afcceab242e57aa67c5afc
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-02-28 18:52:30 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -52,10 +52,10 @@ async def video(c, m):
           return
       else:
           pass
-  if m.from_user.id in Config.BANNED_USER:
+  if m.from_user.id in Config.BANNED_USERS:
       await c.send_message(chat_id=m.chat.id, text=Translation.BANNED_TEXT)
       return
-  if m.from_user.id not in Config.BANNED_USER:
+  if m.from_user.id not in Config.BANNED_USERS:
       if m.reply_to_message is not None:
           await download(c, m)
       else:
@@ -70,9 +70,9 @@ async def file(c, m):
           return
       else:
           pass
-  if m.from_user.id in Config.BANNED_USER:
+  if m.from_user.id in Config.BANNED_USERS:
       await c.send_message(chat_id=m.chat.id, text=Translation.BANNED_TEXT)
-  if m.from_user.id not in Config.BANNED_USER:
+  if m.from_user.id not in Config.BANNED_USERS:
     if m.reply_to_message is not None:
       await download(c, m)
     else:
