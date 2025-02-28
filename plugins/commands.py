0cc659b041894b98d01769b66714abb03f781f54
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-02-28 13:01:47 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -88,14 +88,14 @@ async def login(c, m):
                 await c.send_message(chat_id=m.chat.id,
                                      text=Translation.SUCESS_LOGIN,
                                      disable_web_page_preview=True,
-                                     reply_to_message_id=m.message_id,
+                                     reply_to_message_id = m.id,
                                      parse_mode="markdown")
                 return Config.LOGGED_USER.append(m.from_user.id)
             if str(password) != str(Config.BOT_PWD):
                 await c.send_message(chat_id=m.chat.id,
                                      text=Translation.WRONG_PWD,
                                      disable_web_page_preview=True,
-                                     reply_to_message_id=m.message_id,
+                                     reply_to_message_id = m.id,
                                      parse_mode="markdown")
 
         if (len(m.command) < 2) & (m.from_user.id not in Config.LOGGED_USER) & (m.from_user.id not in Config.AUTH_USERS):
