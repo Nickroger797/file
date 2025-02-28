3413c7620a5b5086787df12879abc88d0dd5e66a
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-02-28 11:05:56 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,111 +1,38 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
-
+from pymongo import MongoClient
 import os
 
-import threading
-import asyncio
+# Load MongoDB connection URL from an environment variable or config
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://username:password@host:port/database")
 
-from sqlalchemy import Column, Integer, Boolean, String, ForeignKey, UniqueConstraint, func
+# Create MongoDB client
+client = MongoClient(MONGO_URI)
 
+# Select the database (change 'your_database_name' to your actual DB name)
+db = client["your_database_name"]
 
-from config import Config
+print("Connected to MongoDB:", db.name)
 
+# Define collections (equivalent to SQL tables)
+thumbnails = db["thumbnails"]
+settings = db["settings"]
 
-def start() -> scoped_session:
-    engine = create_engine(Config.DB_URI, client_encoding="utf8")
-    BASE.metadata.bind = engine
-    BASE.metadata.create_all(engine)
-    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+# Function to add a thumbnail
+def add_thumb(id, msg_id):
+    thumbnails.update_one({"_id": id}, {"$set": {"msg_id": msg_id}}, upsert=True)
 
+# Function to delete a thumbnail
+def del_thumb(id):
+    thumbnails.delete_one({"_id": id})
 
-BASE = declarative_base()
-SESSION = start()
+# Function to get a thumbnail
+def get_thumb(id):
+    return thumbnails.find_one({"_id": id})
 
-INSERTION_LOCK = threading.RLock()
+# Function to update settings
+def update_setting(id, value):
+    settings.update_one({"_id": id}, {"$set": {"value": value}}, upsert=True)
 
-class Thumbnail(BASE):
-    __tablename__ = "thumbnail"
-    id = Column(Integer, primary_key=True)
-    msg_id = Column(Integer)
+# Function to get a setting
+def get_setting(id):
+    return settings.find_one({"_id": id})
     
-    def __init__(self, id, msg_id):
-        self.id = id
-        self.msg_id = msg_id
-
-Thumbnail.__table__.create(checkfirst=True)
-
-async def df_thumb(id, msg_id):
-    with INSERTION_LOCK:
-        msg = SESSION.query(Thumbnail).get(id)
-        if not msg:
-            msg = Thumbnail(id, msg_id)
-            SESSION.add(msg)
-            SESSION.flush()
-        else:
-            SESSION.delete(msg)
-            file = Thumbnail(id, msg_id)
-            SESSION.add(file)
-        SESSION.commit()
-
-async def del_thumb(id):
-    with INSERTION_LOCK:
-        msg = SESSION.query(Thumbnail).get(id)
-        SESSION.delete(msg)
-        SESSION.commit()
-
-async def get_thumb(id):
-    try:
-        t = SESSION.query(Thumbnail).get(id)
-        return t
-    finally:
-        SESSION.close()
-def start() -> scoped_session:
-    engine = create_engine(Config.DB_URI, client_encoding="utf8")
-    BASE.metadata.bind = engine
-    BASE.metadata.create_all(engine)
-    return scoped_session(sessionmaker(bind=engine, autoflush=False))
-
-
-BASE = declarative_base()
-SESSION = start()
-
-INSERTION_LOCK = threading.RLock()
-
-class Settings(BASE):
-    __tablename__ = "settings"
-    id = Column(Integer, primary_key=True)
-    value = Column(Integer)
-    
-    def __init__(self, id, value):
-        self.id = id
-        self.value = value
-
-Settings.__table__.create(checkfirst=True)
-
-async def add(id, value):
-    with INSERTION_LOCK:
-        g = SESSION.query(Settings).get(id)
-        if not g:
-            g = Settings(id, value)
-            SESSION.add(1)
-            SESSION.flush()
-        else:
-            SESSION.delete(g)
-            fil = Settings(id, value)
-            SESSION.add(1)
-        SESSION.commit()
-
-async def remove(id):
-    with INSERTION_LOCK:
-        g = SESSION.query(Settings).get(id)
-        SESSION.delete(g)
-        SESSION.commit()
-
-async def check(id):
-    try:
-        y = SESSION.query(Settings).get(id)
-        return y
-    finally:
-        SESSION.close()
