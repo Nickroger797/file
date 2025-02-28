0121ba5971d079ed089986cec1b415c7bd04d321
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-02-28 22:59:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,14 @@
 echo "Cloning Repo..."
+if [ -d "/file" ]; then
+    rm -rf /file  # Remove old repo if exists
+fi
 git clone https://github.com/Nickroger797/file /file
 cd /file
 
 pip3 install -r requirements.txt
 
 echo "Starting Server..."
-gunicorn --bind 0.0.0.0:8080 server:app &  # Run Flask using Gunicorn
+gunicorn --bind 0.0.0.0:$PORT server:app &
 
 echo "Starting Bot..."
 python3 bot.py  # Run bot normally
