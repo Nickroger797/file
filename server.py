22fc884944b01dbee6684e790abfdc976e85ac4a
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-02-28 15:39:32 +0530

    Update server.py

--- server.py
+++ server.py
@@ -3,9 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def health_check():
-    return "OK", 200
+def home():
+    return "Bot is running!", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
-  
