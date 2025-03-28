b87d513b73bb3ff4149bd5211c50c41e65855b42
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-03-29 01:29:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,40 +1,41 @@
-from database.database import db 
+from database.database import db  
+import pyrogram  
+from pyrogram import Client  
+import logging  
+import os  
+import threading  
+import server  # Flask server ko import kar liya
 
-import pyrogram
+from config import Config  
 
-from pyrogram import Client 
+logging.basicConfig(level=logging.DEBUG,  
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')  
+logger = logging.getLogger(__name__)  
 
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)  
 
-import os
+if __name__ == "__main__":  
+    # Ensure download directory exists  
+    if not os.path.isdir(Config.DOWNLOAD_LOCATION):  
+        os.makedirs(Config.DOWNLOAD_LOCATION)  
 
-from config import Config
+    # Bot user collection in MongoDB  
+    bot_owner_id = 1337144652  # Replace with actual owner ID  
+    db.users.update_one({"_id": bot_owner_id}, {"$set": {"role": "admin"}}, upsert=True)  
 
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
+    plugins = dict(root="plugins")  
 
+    app = Client(  
+        "CONVERTBOT",  
+        bot_token=Config.TG_BOT_TOKEN,  
+        api_id=Config.APP_ID,  
+        api_hash=Config.API_HASH,  
+        plugins=plugins  
+    )  
 
-if __name__ == "__main__":
-    # Ensure download directory exists
-    if not os.path.isdir(Config.DOWNLOAD_LOCATION):
-        os.makedirs(Config.DOWNLOAD_LOCATION)
+    Config.AUTH_USERS.add(bot_owner_id)  # Keep authentication list  
 
-    # Bot user collection in MongoDB
-    bot_owner_id = 1337144652  # Replace with actual owner ID
-    db.users.update_one({"_id": bot_owner_id}, {"$set": {"role": "admin"}}, upsert=True)
+    # Flask server ko alag thread me chalane ke liye  
+    threading.Thread(target=server.run).start()  
 
-    plugins = dict(root="plugins")
-
-    app = Client(
-        "CONVERTBOT",
-        bot_token=Config.TG_BOT_TOKEN,
-        api_id=Config.APP_ID,
-        api_hash=Config.API_HASH,
-        plugins=plugins
-    )
-
-    Config.AUTH_USERS.add(bot_owner_id)  # Keep authentication list
     app.run()
-
